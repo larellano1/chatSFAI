71b27e1af447fe7abfc3c07dec8f1d0490028e44
Luis Arellano <lfv.arellano@gmail.com>
2023-01-17 15:10:24 -0300

    Incluindo arquivo com dados de "conhecimento prévio".

--- chatSFAI/chatSFAI.py
+++ chatSFAI/chatSFAI.py
@@ -12,7 +12,7 @@ opening = "Preste atenção no que eu vou te dizer, pois vou te contar o que eu 
 response = openai.Completion.create(engine="text-davinci-002", prompt=opening)
 print(response["choices"][0]["text"])
 
-data_file = "C:\\Users\\d805664\\Documents\\chatSFAI\\chatSFAI\\myproject\\myproject\\spiders\\data.json"
+data_file = "data.json"
 informacao = open(data_file).read()
 chunk_size = 4000
 informacao_chunks = [informacao[i:i+chunk_size] for i in range(0, len(informacao), chunk_size)]
